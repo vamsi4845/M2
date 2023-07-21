ab3ff90ee31c3eec82e354a36a5681477feae6bd
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2023-07-21 11:40:31 -0700

    query updates.

--- rollups/sovereign-labs/msl/modules/sov-aptosvm/src/query.rs
+++ rollups/sovereign-labs/msl/modules/sov-aptosvm/src/query.rs
@@ -1,18 +1,17 @@
-/*use anvil_core::eth::transaction::EthTransactionRequest;
+use anvil_core::eth::transaction::EthTransactionRequest;
 use ethereum_types::{Address, H256, U256, U64};
 use ethers_core::types::{Block, BlockId, FeeHistory, Transaction, TransactionReceipt, TxHash};
 use sov_modules_macros::rpc_gen;
 use sov_state::WorkingSet;
 use tracing::info;
 
-use crate::Evm;
+use crate::AptosVm;
 
-#[rpc_gen(client, server, namespace = "eth")]
-impl<C: sov_modules_api::Context> Evm<C> {
+#[rpc_gen(client, server, namespace = "aptos")]
+impl<C: sov_modules_api::Context> AptosVm<C> {
     #[rpc_method(name = "chainId")]
     pub fn chain_id(&self, _working_set: &mut WorkingSet<C::Storage>) -> Option<U64> {
-        info!("evm module: eth_chainId");
-        Some(U64::from(1u64))
+        unimplemented!("aptos_chainId not implemented")
     }
 
     #[rpc_method(name = "getBlockByNumber")]
@@ -22,25 +21,12 @@ impl<C: sov_modules_api::Context> Evm<C> {
         _details: Option<bool>,
         _working_set: &mut WorkingSet<C::Storage>,
     ) -> Option<Block<TxHash>> {
-        info!("evm module: eth_getBlockByNumber");
-
-        let block = Block::<TxHash> {
-            base_fee_per_gas: Some(100.into()),
-            ..Default::default()
-        };
-
-        Some(block)
+        unimplemented!("aptos_getBlockByNumber not implemented")
     }
 
     #[rpc_method(name = "feeHistory")]
     pub fn fee_history(&self, _working_set: &mut WorkingSet<C::Storage>) -> FeeHistory {
-        info!("evm module: eth_feeHistory");
-        FeeHistory {
-            base_fee_per_gas: Default::default(),
-            gas_used_ratio: Default::default(),
-            oldest_block: Default::default(),
-            reward: Default::default(),
-        }
+        unimplemented!("aptos_getFeeHistory not implemented")
     }
 
     #[rpc_method(name = "sendTransaction")]
@@ -49,12 +35,12 @@ impl<C: sov_modules_api::Context> Evm<C> {
         _request: EthTransactionRequest,
         _working_set: &mut WorkingSet<C::Storage>,
     ) -> U256 {
-        unimplemented!("eth_sendTransaction not implemented")
+        unimplemented!("aptos_sendTransaction not implemented")
     }
 
     #[rpc_method(name = "blockNumber")]
     pub fn block_number(&self, _working_set: &mut WorkingSet<C::Storage>) -> U256 {
-        unimplemented!("eth_blockNumber not implemented")
+        unimplemented!("aptos_blockNumber not implemented")
     }
 
     #[rpc_method(name = "getTransactionByHash")]
@@ -63,9 +49,7 @@ impl<C: sov_modules_api::Context> Evm<C> {
         hash: H256,
         working_set: &mut WorkingSet<C::Storage>,
     ) -> Option<Transaction> {
-        info!("evm module: eth_getTransactionByHash");
-        let evm_transaction = self.transactions.get(&hash.into(), working_set);
-        evm_transaction.map(|tx| tx.into())
+        unimplemented!("aptos_getTransactionReceipt not implemented")
     }
 
     #[rpc_method(name = "getTransactionReceipt")]
@@ -74,7 +58,7 @@ impl<C: sov_modules_api::Context> Evm<C> {
         _hash: H256,
         _working_set: &mut WorkingSet<C::Storage>,
     ) -> Option<TransactionReceipt> {
-        unimplemented!("eth_getTransactionReceipt not implemented")
+        unimplemented!("aptos_getTransactionReceipt not implemented")
     }
 
     #[rpc_method(name = "getTransactionCount")]
@@ -84,6 +68,6 @@ impl<C: sov_modules_api::Context> Evm<C> {
         _block_number: Option<BlockId>,
         _working_set: &mut WorkingSet<C::Storage>,
     ) -> Option<U256> {
-        unimplemented!("eth_getTransactionCount not implemented")
+        unimplemented!("aptos_getTransactionCount not implemented")
     }
-}*/
+}
