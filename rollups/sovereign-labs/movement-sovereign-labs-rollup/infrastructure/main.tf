02adca6c98070eee2ae1b3a848350a69278fadf9
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2023-08-02 13:42:19 -0400

    bad submodule.

--- rollups/sovereign-labs/movement-sovereign-labs-rollup/infrastructure/main.tf
+++ rollups/sovereign-labs/movement-sovereign-labs-rollup/infrastructure/main.tf
@@ -6,18 +6,28 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_subnet" "fargate_subnet" {
+resource "aws_subnet" "fargate_subnet1" {
   vpc_id            = data.aws_vpc.default.id
   cidr_block        = "172.31.48.0/20"
   availability_zone = "us-east-2a"
 
   tags = {
-    Name = "fargate_subnet"
+    Name = "fargate_subnet1"
+  }
+}
+
+resource "aws_subnet" "fargate_subnet2" {
+  vpc_id            = data.aws_vpc.default.id
+  cidr_block        = "172.31.64.0/20"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "fargate_subnet2"
   }
 }
 
 locals {
-  subnet_ids = [aws_subnet.fargate_subnet.id]
+  subnet_ids = [aws_subnet.fargate_subnet1.id, aws_subnet.fargate_subnet2.id]
 }
 
 resource "aws_ecs_cluster" "cluster" {
@@ -81,10 +91,17 @@ resource "aws_ecs_service" "service" {
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.target_group.arn
+    target_group_arn = aws_lb_target_group.tg_http.arn
     container_name   = "sov_monovm"
     container_port   = 8000
   }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg_https.arn
+    container_name   = "sov_monovm"
+    container_port   = 443
+  }
+
 }
 
 resource "aws_iam_role" "ecs_role" {
@@ -167,59 +184,46 @@ resource "aws_acm_certificate_validation" "cert" {
 }
 
 resource "aws_lb" "lb" {
-  name               = "fargate-lb"
+  name               = "my-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
+  security_groups    = [aws_security_group.sg.id]
   subnets            = local.subnet_ids
 }
 
-resource "aws_lb_listener" "listener" {
+resource "aws_lb_target_group" "tg_https" {
+  name     = "tghttps"
+  port     = 12345
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+}
+
+resource "aws_lb_target_group" "tg_http" {
+  name     = "tghttp"
+  port     = 12345
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+}
+
+resource "aws_lb_listener" "listener_https" {
   load_balancer_arn = aws_lb.lb.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = aws_acm_certificate_validation.cert.certificate_arn
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.target_group.arn
+    target_group_arn = aws_lb_target_group.tg_https.arn
   }
 }
 
+resource "aws_lb_listener" "listener_http" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "8000"
+  protocol          = "HTTP"
 
-resource "aws_lb_target_group" "target_group" {
-  name     = "fargate-tg"
-  port     = 8000
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    enabled             = true
-    interval            = 30
-    path                = "/"
-    timeout             = 3
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-  }
-}
-
-resource "aws_security_group" "lb_sg" {
-  name        = "lb_sg"
-  description = "LB security group"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg_http.arn
   }
 }
